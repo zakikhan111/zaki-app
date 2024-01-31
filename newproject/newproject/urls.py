4b9c39b5aae0349155d379440d3201534e87fc70
zaki khan <143845544+zakikhan111@users.noreply.github.com>
2024-01-31 15:19:55 +0000

    update changes

--- newproject/newproject/urls.py
+++ newproject/newproject/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from newapp import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("name/",views.food),
 ]
